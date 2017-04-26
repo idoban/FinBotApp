762d6fff88c86ffd58d80f8f8dd60cec40a33140
Boris Modylevsky <boris.modylevsky@citi.com>
2017-04-26 17:34:04 +0300

    move text to siml project

--- FinBot/Engine/PaymentsAmountAdapter.cs
+++ FinBot/Engine/PaymentsAmountAdapter.cs
@@ -18,11 +18,9 @@ namespace FinBot.Engine
             XText productXText = context.Element.Nodes().OfType<XText>().FirstOrDefault();
             if (productXText == null)
             {
-                return "What product do you want to know how many payments left?";
+                return "";
             }
-            var remainingPayments = _financialServices.GetRemainingPayments(productXText.Value);
-
-            return string.Format("You have {0} remaining payments for {1}", remainingPayments, productXText.Value);
+            return _financialServices.GetRemainingPayments(productXText.Value).ToString();
         }
     }
 }
